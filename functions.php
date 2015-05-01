82b9be658bbab35f283e0eace17d80dbc2c292ed
Cody W <cody@codywhitby.net>
2015-05-01 12:19:56 -0400

    init

---
+++ functions.php
@@ -0,0 +1,328 @@
+<?php
+/*
+Author: Cody Whitby
+URL: http://themble.com/bones/
+
+This is where you can drop your custom functions or
+just edit things like thumbnail sizes, header images,
+sidebars, comments, ect.
+*/
+
+// LOAD BONES CORE (if you remove this, the theme will break)
+require_once( 'library/bones.php' );
+// LOAD TGM Plugin Class
+require_once( 'library/tgm-plugin-activation.php' );
+
+// CUSTOMIZE THE WORDPRESS ADMIN (off by default)
+// require_once( 'library/admin.php' );
+
+/*********************
+LAUNCH BONES
+Let's get everything up and running.
+*********************/
+
+function bones_ahoy() {
+
+  //Allow editor style.
+  add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
+
+  // let's get language support going, if you need it
+  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+
+  // USE THIS TEMPLATE TO CREATE CUSTOM POST TYPES EASILY
+  require_once( 'library/custom-post-type.php' );
+
+  // launching operation cleanup
+  add_action( 'init', 'bones_head_cleanup' );
+  // A better title
+  add_filter( 'wp_title', 'rw_title', 10, 3 );
+  // remove WP version from RSS
+  add_filter( 'the_generator', 'bones_rss_version' );
+  // clean up gallery output in wp
+  add_filter( 'gallery_style', 'bones_gallery_style' );
+
+  // enqueue base scripts and styles
+  add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+  // ie conditional wrapper
+
+  // launching this stuff after theme setup
+  bones_theme_support();
+
+  // adding sidebars to Wordpress (these are created in functions.php)
+  add_action( 'widgets_init', 'bones_register_sidebars' );
+
+  // cleaning up random code around images
+  add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+  // cleaning up excerpt
+  add_filter( 'excerpt_more', 'bones_excerpt_more' );
+
+} /* end bones ahoy */
+
+// let's get this party started
+add_action( 'after_setup_theme', 'bones_ahoy' );
+
+
+/*********************
+TGMPA Plugin Activation
+*********************/
+
+add_action( 'tgmpa_register', 'bones_register_required_plugins' );
+/**
+ * Register the required plugins for this theme.
+ */
+function bones_register_required_plugins() {
+
+    /**
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
+    $plugins = array(
+
+        // This is an example of how to include a plugin from a private repo in your theme.
+        array(
+            'name'               => 'Advanced Custom Fields Pro', // The plugin name.
+            'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
+            'source'             => 'http://arts.vcu.edu/dev/advanced-custom-fields-pro.zip', // The plugin source.
+            'required'           => false, // If false, the plugin is only 'recommended' instead of required.
+        ),
+
+        // This is an example of how to include a plugin from the WordPress Plugin Repository.
+        array(
+            'name'      => 'WordPress Importer',
+            'slug'      => 'wordpress-importer',
+            'required'  => false,
+        ),
+        array(
+            'name'      => 'Debug Bar',
+            'slug'      => 'debug-bar',
+            'required'  => false,
+        )
+
+    );
+
+    /**
+     * Array of configuration settings. Amend each line as needed.
+     * If you want the default strings to be available under your own theme domain,
+     * leave the strings uncommented.
+     * Some of the strings are added into a sprintf, so see the comments at the
+     * end of each line for what each argument will be.
+     */
+    $config = array(
+        'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+        'message'      => '',                      // Message to output right before the plugins table.
+        'strings'      => array(
+            'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
+            'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
+            'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
+            'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
+            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+            'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
+            'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
+            'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+            'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+        )
+    );
+
+    tgmpa( $plugins, $config );
+
+}
+
+
+/************* OEMBED SIZE OPTIONS *************/
+
+if ( ! isset( $content_width ) ) {
+	$content_width = 640;
+}
+
+/************* THUMBNAIL SIZE OPTIONS *************/
+
+// Thumbnail sizes
+add_image_size( 'bones-thumb-600', 600, 150, true );
+add_image_size( 'bones-thumb-300', 300, 100, true );
+
+/*
+to add more sizes, simply copy a line from above
+and change the dimensions & name. As long as you
+upload a "featured image" as large as the biggest
+set width or height, all the other sizes will be
+auto-cropped.
+
+To call a different size, simply change the text
+inside the thumbnail function.
+
+For example, to call the 300 x 100 sized image,
+we would use the function:
+<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+for the 600 x 150 image:
+<?php the_post_thumbnail( 'bones-thumb-600' ); ?>
+
+You can change the names and dimensions to whatever
+you like. Enjoy!
+*/
+
+add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
+
+function bones_custom_image_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'bones-thumb-600' => __('600px by 150px'),
+        'bones-thumb-300' => __('300px by 100px'),
+    ) );
+}
+
+/*
+The function above adds the ability to use the dropdown menu to select
+the new images sizes you have just created from within the media manager
+when you add media to your content blocks. If you add more image sizes,
+duplicate one of the lines in the array and name it according to your
+new image size.
+*/
+
+/************* THEME CUSTOMIZE *********************/
+
+/* 
+  A good tutorial for creating your own Sections, Controls and Settings:
+  http://code.tutsplus.com/series/a-guide-to-the-wordpress-theme-customizer--wp-33722
+  
+  Good articles on modifying the default options:
+  http://natko.com/changing-default-wordpress-theme-customization-api-sections/
+  http://code.tutsplus.com/tutorials/digging-into-the-theme-customizer-components--wp-27162
+  
+  To do:
+  - Create a js for the postmessage transport method
+  - Create some sanitize functions to sanitize inputs
+  - Create some boilerplate Sections, Controls and Settings
+*/
+
+function bones_theme_customizer($wp_customize) {
+  // $wp_customize calls go here.
+  //
+  // Uncomment the below lines to remove the default customize sections 
+
+  // $wp_customize->remove_section('title_tagline');
+  // $wp_customize->remove_section('colors');
+  // $wp_customize->remove_section('background_image');
+  // $wp_customize->remove_section('static_front_page');
+  // $wp_customize->remove_section('nav');
+
+  // Uncomment the below lines to remove the default controls
+  // $wp_customize->remove_control('blogdescription');
+  
+  // Uncomment the following to change the default section titles
+  // $wp_customize->get_section('colors')->title = __( 'Theme Colors' );
+  // $wp_customize->get_section('background_image')->title = __( 'Images' );
+}
+
+add_action( 'customize_register', 'bones_theme_customizer' );
+
+/************* ACTIVE SIDEBARS ********************/
+
+// Sidebars & Widgetizes Areas
+function bones_register_sidebars() {
+	register_sidebar(array(
+		'id' => 'sidebar1',
+		'name' => __( 'Sidebar 1', 'bonestheme' ),
+		'description' => __( 'The first (primary) sidebar.', 'bonestheme' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	/*
+	to add more sidebars or widgetized areas, just copy
+	and edit the above sidebar code. In order to call
+	your new sidebar just use the following code:
+
+	Just change the name to whatever your new
+	sidebar's id is, for example:
+
+	register_sidebar(array(
+		'id' => 'sidebar2',
+		'name' => __( 'Sidebar 2', 'bonestheme' ),
+		'description' => __( 'The second (secondary) sidebar.', 'bonestheme' ),
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h4 class="widgettitle">',
+		'after_title' => '</h4>',
+	));
+
+	To call the sidebar in your template, you can just copy
+	the sidebar.php file and rename it to your sidebar's name.
+	So using the above example, it would be:
+	sidebar-sidebar2.php
+
+	*/
+} // don't remove this bracket!
+
+
+/*
+This is a modification of a function found in the
+twentythirteen theme where we can declare some
+external fonts. If you're using Google Fonts, you
+can replace these fonts, change it in your scss files
+and be up and running in seconds.
+*/
+function bones_fonts() {
+  wp_enqueue_style('googleFonts', 'http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic');
+}
+
+add_action('wp_enqueue_scripts', 'bones_fonts');
+
+// Enable support for HTML5 markup.
+	add_theme_support( 'html5', array(
+		'search-form',
+	) );
+
+/*
+we_are_live() is a function for testing our environment. 
+@returns true if on production server false if not
+*/
+
+function we_are_live(){
+  $current_server = $_SERVER['HTTP_HOST']; 
+
+  if ( $current_server == 'arts.vcu.edu' ){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+get_development_scripts() tests our environment with we_are_live()
+@returns the appropriate scripts if local
+
+use [grunt sync] to get ports for the last three scripts
+*/
+function get_development_scripts(){
+
+  if ( !we_are_live() ){
+    include( 'library/dev-scripts.php' );
+  }
+
+}
+
+
+
+// Enable support for HTML5 markup.
+	add_theme_support( 'html5', array(
+		'comment-list',
+		'search-form',
+		'comment-form'
+	) );
+
+/* DON'T DELETE THIS CLOSING TAG */ ?>
---
+++ readme.md
@@ -0,0 +1,27 @@
+*Installation (only required if not using vvvcuarts vagrant)*
+```bash
+npm install
+```
+
+```bash
+bower install
+```
+
+*Usage:*
+
+```bash
+grunt init
+```
+
+```bash
+grunt dev
+```
+
+```bash
+grunt build
+```
+
+## Bones
+![dance](http://i.imgur.com/YuP1ruw.gif)
+
+
