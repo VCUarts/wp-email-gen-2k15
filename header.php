ae667c2b6339147e51498d6b1a20067423d033b5
Cody W <cody@codywhitby.net>
2015-05-01 16:28:43 -0400

    and some email meta stuff

--- header.php
+++ header.php
@@ -3,15 +3,10 @@
 	<head>
 		<meta charset="utf-8">
 
-		<?php // force Internet Explorer to use the latest rendering engine available ?>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
 		<title><?php wp_title(''); ?></title>
 
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 		<?php // wordpress head functions ?>
 		<?php wp_head(); ?>
