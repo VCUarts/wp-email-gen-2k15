0f06aaa6755b311404b91edcea9c9ef1623d5434
Cody W <cody@codywhitby.net>
2015-05-07 16:03:03 -0400

    rollback and include csstoinline lib

--- single.php
+++ single.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
   <?php the_content();?>
+
+  <?php 
+    require_once __DIR__ . '/vendor/autoload.php';
+    use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+    // create instance
+    $cssToInlineStyles = new CssToInlineStyles();
+    $html = file_get_contents(__DIR__ . '/example.html');
+    $css = file_get_contents(__DIR__ . '/example.css');
+    $cssToInlineStyles->setHTML($html);
+    $cssToInlineStyles->setCSS($css);
+    // output
+    echo $cssToInlineStyles->convert();
+
+    ?>
   
 <?php get_footer(); ?>
