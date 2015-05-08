42825b9af1437f77f11e78a644f3eee16052ae2e
Cody W <cody@codywhitby.net>
2015-05-08 14:24:55 -0400

    hero image & caption module, styles

--- library/flexible-content/hero-module.php
+++ library/flexible-content/hero-module.php
@@ -5,5 +5,6 @@ $caption = get_sub_field('hero_caption'); ?>
 
 <img class="image_fix" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" width="<?php echo $image['width']; ?>" height="<?php echo $image['height']; ?>" />
 
-<?php
- ?>
+<?php if ($caption): ?> 
+  <div class="img-caption"><?php echo $caption; ?></div>
+<?php endif; ?>
