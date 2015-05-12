13b2448305a9395681282dd074edbd8dfd98cea3
Cody W <cody@codywhitby.net>
2015-05-12 15:59:34 -0400

    hero module wrap

--- library/flexible-content/hero-module.php
+++ library/flexible-content/hero-module.php
@@ -3,8 +3,10 @@
 $image = get_sub_field('hero_image');
 $caption = get_sub_field('hero_caption'); ?>
 
-<img class="image_fix" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" width="<?php echo $image['width']; ?>" height="<?php echo $image['height']; ?>" />
+<div class="hero-module">
+  <img class="image_fix" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" width="<?php echo $image['width']; ?>" height="<?php echo $image['height']; ?>" />
 
-<?php if ($caption): ?> 
-  <div class="img-caption"><?php echo $caption; ?></div>
-<?php endif; ?>
+  <?php if ($caption): ?> 
+    <div class="img-caption"><?php echo $caption; ?></div>
+  <?php endif; ?>
+</div>
