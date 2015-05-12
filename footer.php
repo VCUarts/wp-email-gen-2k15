2c449e6481168f2aee5c5ce2cdddb2591b6f4a4a
Cody W <cody@codywhitby.net>
2015-05-12 14:15:16 -0400

    footer init

--- footer.php
+++ footer.php
@@ -1,4 +1,34 @@
-	<?php // all js scripts are loaded in library/bones.php ?>
+	<div class="footer">
+   <table cellpadding="0" cellspacing="0" border="0" align="center" class="head-wrap">
+     <tr>
+       <td></td>
+       <td class="footer container">
+
+           <div class="content">
+           <table>
+             <tr>
+               <td>
+                 <p>
+                   Virginia Commonwealth University | School of the Arts <br>
+                   325 North Harrison Street, PO Box 842519 | Richmond, VA 23284-2519 <br>
+                   Phone: 804-VCU-ARTS (804-828-2787) | 866-534-3201 (toll free) | Fax: 804-828-6469 <br>
+                   <a href="mailto:arts@vcu.edu" target="_blank">arts@vcu.edu</a>
+                 </p>
+                 <p>
+                   <a href="http://www.vcu.edu/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/vcu-logo.png"></a>
+                 </p>
+               </td>
+             </tr>
+           </table>
+           </div>
+
+       </td>
+       <td></td>
+     </tr>
+   </table>
+  </div>
+
+  <?php // all js scripts are loaded in library/bones.php ?>
 	<?php wp_footer(); ?>
 
   <?php echo get_development_scripts(); ?>
