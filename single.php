52e63e84caace71241b217c529adebce913e3d76
Cody W <cody@codywhitby.net>
2015-05-13 13:46:12 -0400

    chunks workin and twerkin

--- single.php
+++ single.php
@@ -90,6 +90,39 @@
                  <a href="mailto:arts@vcu.edu" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/email.png"></a>
                </div>
 
+               <?php 
+
+               $chunks = get_field('chunks');
+               $chunkCount = 0;
+
+               if( $chunks ): ?>
+               <div class="chunks">
+               <table cellpadding="0" cellspacing="0" border="0" align="center">
+                 <tr>
+                  <?php foreach( $chunks as $post): // variable must be called $post (IMPORTANT) ?>
+                    <?php setup_postdata($post);
+                      $image = get_field('chunk_image'); 
+                      $chunk_title = get_field('chunk_headline');
+                      $chunk_content = get_field('chunk_content'); 
+                      $chunk_link = get_field('more_link'); ?>
+
+                    <?php if ($chunkCount++ % 2 == 1 ): ?>
+                      <td class="column column-two" width="265" valign="top">
+                    <?php else: ?>
+                      <td class="column column-one" width="265" valign="top">
+                    <?php endif; ?>
+                       <?php if ($image):?><img src="<?php echo $image['url'];?>"><?php endif; ?>
+                       <?php if ($chunk_title):?><h2 class="chunk-title"><?php echo $chunk_title; ?></h2><?php endif; ?>
+                       <?php if ($chunk_content): echo $chunk_content; endif; ?>
+                       <?php if ($chunk_link): ?><a href="<?php echo $chunk_link; ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/more-btn.png" /></a> <?php endif; ?>
+                      </td>
+                  <?php endforeach; ?>
+                  <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+                 </tr>
+               </table>
+               </div>
+               <?php endif; ?>
+
                <div class="how-to-apply">
                  <a href="http://arts.vcu.edu/admissions/how-to-apply/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/howtoapply.png"></a>
                </div>
