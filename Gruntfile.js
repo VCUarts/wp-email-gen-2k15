d398642a799d6f99f22238de1fa5fe992226f70e
Cody W <cody@codywhitby.net>
2015-05-21 16:05:39 -0400

    cleaning up grunt fluff

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,14 +25,10 @@ module.exports = function(grunt) {
         watch: {
             scss: {
                 files: ['library/scss/**/*.scss'],
-                tasks: ['sass']
+                tasks: ['sass', 'autoprefixer', 'cssmin']
             },
             css: {
                 files: ['library/css/**/*.css']
-            },
-            js: {
-                files: ['library/js/**/*.js','!library/js/dist/**/*.js'],
-                tasks: ['concat']
             },
             livereload: {
                 files: ['**/*.html', '**/*.php', '**/*.js', '**/*.css', '!**/node_modules/**'],
@@ -48,57 +44,16 @@ module.exports = function(grunt) {
             }
         },
  
-        cmq: {
-            your_target: {
-                files: {
-                    'library/css' : 'library/css/style.css'
-                }
-            }
-        },
- 
         cssmin: {
+            options: {
+                report: 'min',
+                keepBreaks: true,
+                keepSpecialComments: 0
+            },
             combine: {
                 files: {
-                    'library/css/style.min.css': ['library/css/style.css']
+                    'library/css/style.css': ['library/css/style.css']
                 }
-            }
-        },
- 
-        jshint: {
-            all: [
-                'library/js/*.js',
-            ],
-            options: {
-                jshintrc: 'library/js/.jshintrc'
-            }
-        },
- 
-        concat: {  
-            footer: {
-                src: [
-                    'library/js/libs/*.js', // All JS in the libs folder
-                    'library/js/scripts.js',  // This specific file
-                    '!library/js/libs/modernizr.custom.min.js'
-                ],
-                dest: 'library/js/dist/main.js',
-            }
-        },
- 
-        uglify: {
-            footer: {
-                src: 'library/js/dist/main.js',
-                dest: 'library/js/dist/main.min.js'
-            }
-        },
- 
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'library/images/',
-                    src: ['**/*.{png,jpg,gif,svg,ico}'],
-                    dest: 'library/images/'
-                }]
             }
         },
 
@@ -117,16 +72,7 @@ module.exports = function(grunt) {
     // Sass
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-combine-media-queries');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
- 
-    // JS
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
- 
-    // Images
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
  
     // Browser Reload + File Watch
     grunt.loadNpmTasks('grunt-concurrent');
@@ -135,5 +81,5 @@ module.exports = function(grunt) {
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('init', ['build']);
     grunt.registerTask('dev', ['watch']);
-    grunt.registerTask('build', ['sass', 'autoprefixer', 'cmq', 'cssmin', 'concat', 'uglify']);
+    grunt.registerTask('build', ['sass', 'autoprefixer', 'cssmin']);
 };
