959855621a4060d9fbf027094bec10e566aab53a
Cody W <cody@codywhitby.net>
2015-07-01 14:31:54 -0400

    vcuarts red

--- functions.php
+++ functions.php
@@ -182,6 +182,62 @@ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 
+/*
+** Adding VCUarts Red to TinyMCE colors
+*/
+function emailgen_change_tinymce_colors( $init ) {
+    $default_colours = '
+        "000000", "Black",
+        "993300", "Burnt orange",
+        "333300", "Dark olive",
+        "003300", "Dark green",
+        "003366", "Dark azure",
+        "000080", "Navy Blue",
+        "333399", "Indigo",
+        "333333", "Very dark gray",
+        "800000", "Maroon",
+        "FF6600", "Orange",
+        "808000", "Olive",
+        "008000", "Green",
+        "008080", "Teal",
+        "0000FF", "Blue",
+        "666699", "Grayish blue",
+        "808080", "Gray",
+        "FF0000", "Red",
+        "FF9900", "Amber",
+        "99CC00", "Yellow green",
+        "339966", "Sea green",
+        "33CCCC", "Turquoise",
+        "3366FF", "Royal blue",
+        "800080", "Purple",
+        "999999", "Medium gray",
+        "FF00FF", "Magenta",
+        "FFCC00", "Gold",
+        "FFFF00", "Yellow",
+        "00FF00", "Lime",
+        "00FFFF", "Aqua",
+        "00CCFF", "Sky blue",
+        "993366", "Brown",
+        "C0C0C0", "Silver",
+        "FF99CC", "Pink",
+        "FFCC99", "Peach",
+        "FFFF99", "Light yellow",
+        "CCFFCC", "Pale green",
+        "CCFFFF", "Pale cyan",
+        "99CCFF", "Light sky blue",
+        "CC99FF", "Plum",
+        "FFFFFF", "White"
+        ';
+    $custom_colours = '
+        "ee2c42", "VCUarts Red"
+        ';
+    $init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+    $init['textcolor_rows'] = 6; // expand colour grid to 6 rows
+    return $init;
+}
+add_filter('tiny_mce_before_init', 'emailgen_change_tinymce_colors');
+
+
 /**
  * Loading CssToInlineStyles
  */
