8fbdcf15111eebac0f705283dd8eb5120b275c3b
mrndlng <mrndlng@gmail.com>
2016-08-31 17:03:01 -0400

    fancy social icons

--- footer.php
+++ footer.php
@@ -14,19 +14,19 @@
                    <table cellpadding="0" cellspacing="0" border="0" align="center" class="social-table">
                      <tr>
                        <td width="20%">
-                         <a href="https://www.facebook.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/facebook.png"></a>
+                         <a href="https://www.facebook.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/facebook.png" width="13" height="25"></a>
                        </td>
                        <td width="20%">
-                         <a href="https://twitter.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/twitter.png"></a>
+                         <a href="https://twitter.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/twitter.png" width="29" height="24"></a>
                        </td>
                        <td width="20%">
-                         <a href="http://instagram.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/instagram.png"></a>
+                         <a href="http://instagram.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/instagram.png" width="25" height="25"></a>
                        </td>
                        <td width="20%">
-                         <a href="http://www.youtube.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/youtube.png"></a>
+                         <a href="http://www.youtube.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/youtube.png" width="25" height="25"></a>
                        </td>
                        <td width="20%">
-                         <a href="mailto:arts@vcu.edu" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/email_icon.png"></a>
+                         <a href="mailto:arts@vcu.edu" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/email.png" width="26"></a>
                        </td>
                      </tr>
                    </table>
