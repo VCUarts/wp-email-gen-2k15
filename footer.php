8117dc5ca9951a8750a8e94b781ab65d885376b7
Cody <cody@codywhitby.net>
2016-09-14 11:14:40 -0400

    footer fix

--- footer.php
+++ footer.php
@@ -13,19 +13,19 @@
                    <table cellpadding="0" cellspacing="0" border="0" align="center" class="social-table">
                      <tr>
                        <td width="20%">
-                         <a href="https://www.facebook.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/facebook.png" width="13" height="25"></a>
+                         <a href="https://www.facebook.com/vcuarts" target="_blank"><img class="image_fix" src="<?php echo get_template_directory_uri(); ?>/library/images/social/facebook.png" width="13" height="25"></a>
                        </td>
                        <td width="20%">
-                         <a href="https://twitter.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/twitter.png" width="29" height="24"></a>
+                         <a href="https://twitter.com/vcuarts" target="_blank"><img class="image_fix" src="<?php echo get_template_directory_uri(); ?>/library/images/social/twitter.png" width="29" height="24"></a>
                        </td>
                        <td width="20%">
-                         <a href="http://instagram.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/instagram.png" width="25" height="25"></a>
+                         <a href="http://instagram.com/vcuarts" target="_blank"><img class="image_fix" src="<?php echo get_template_directory_uri(); ?>/library/images/social/instagram.png" width="25" height="25"></a>
                        </td>
                        <td width="20%">
-                         <a href="http://www.youtube.com/vcuarts" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/youtube.png" width="25" height="25"></a>
+                         <a href="http://www.youtube.com/vcuarts" target="_blank"><img class="image_fix" src="<?php echo get_template_directory_uri(); ?>/library/images/social/youtube.png" width="25" height="25"></a>
                        </td>
                        <td width="20%">
-                         <a href="mailto:arts@vcu.edu" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/library/images/social/email.png" width="26"></a>
+                         <a href="mailto:arts@vcu.edu" target="_blank"><img class="image_fix" src="<?php echo get_template_directory_uri(); ?>/library/images/social/email.png" width="26" height="25"></a>
                        </td>
                      </tr>
                    </table>
